3e7ec7e81c6b410abb4d74302e1fa5e71191617b
nurarifah <aprilioifaah@gmail.com>
2016-01-24 22:38:54 +0800

    Add dynamic Route

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ def about():
 def blogt():
 	return render_template ("blog.html")
 
+@app.route("/blog/<int:post_id>")
+def search(post_id):
+    return 'Post %d'  % post_id
+
 if __name__ == "__main__":
 	app.run()
