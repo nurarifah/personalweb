91652bc242c0c04eee3021a4f6c8f20ef1b9d43c
nurarifah <aprilioifaah@gmail.com>
2016-01-24 23:00:56 +0800

    dynamic route

--- app.py
+++ app.py
@@ -11,12 +11,12 @@ def about():
 	return render_template ("about.html")
 
 @app.route('/blog/')
-def blogt():
+def blog():
 	return render_template ("blog.html")
 
 @app.route("/blog/<int:post_id>")
 def search(post_id):
-    return 'Post %d'  % post_id
+    return render_template ("blog01.html")
 
 if __name__ == "__main__":
 	app.run()
